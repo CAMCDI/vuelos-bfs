364a8d106384d907f146de352e63b3617bebdc51
Cesar Antonio mtz <xdc9125@gmail.com>
2026-03-19 08:40:17 -0600

    fix: agregar whitenoise storage para estáticos

--- pyproject/settings.py
+++ pyproject/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
